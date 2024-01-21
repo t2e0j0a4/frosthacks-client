4f9de6c9485561e5cbb1e96f49b811201ef8aa01
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-01-21 17:03:28 +0530

    check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import AppState from '@/context/AppContext';
 const poppins = Poppins({ subsets: ['latin'], weight: ["100", "300", "500", "700"] });
 
 export const metadata: Metadata = {
-  title: 'Web Portal | Kode Blue Technologies',
-  description: 'Live emergency cases Dashboard for Kode Blue Hospitals.',
+  title: 'Web Portal |  Technologies',
+  description: 'Live emergency cases Dashboard for  Hospitals.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
